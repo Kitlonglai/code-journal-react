f48dbda4af4695485ec6c66cf2cfd8b87e27313e
Kitlong Lai <thekitlong@gmail.com>
2023-10-04 03:56:19 +0000

    directory testing

--- client/src/View.jsx
+++ client/src/View.jsx
@@ -3,6 +3,9 @@ export default function View() {
     <div>
       <h1>Entries</h1>
       <a>New</a>
+      <img src="https://blog.depositphotos.com/wp-content/uploads/2017/07/Soothing-nature-backgrounds-2-1024x684.jpg"></img>
+      <h4>A newer image</h4>
+      <p>A very nice lake!</p>
     </div>
   );
 }
