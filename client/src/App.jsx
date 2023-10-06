e7952f228e4ccfd56ecac8c01572cd78d4d24c1e
Kitlong Lai <thekitlong@gmail.com>
2023-10-06 03:51:05 +0000

    View an Entry #2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import Header from './Header';
 import View from './View';
 
---
+++ client/src/Delete.jsx
@@ -0,0 +1,17 @@
+export default function View() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="column-full">
+          <h1>Entries</h1>
+          <h3>
+            <a>New</a>
+          </h3>
+          <img src="https://blog.depositphotos.com/wp-content/uploads/2017/07/Soothing-nature-backgrounds-2-1024x684.jpg"></img>
+          <h4>A newer image</h4>
+          <p>A very nice lake!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
